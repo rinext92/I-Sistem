bb33888483353a379a0c206dc5d85df4f7cd2473
ruzaini <ahmad.ruzaini@ums.edu.my>
2017-04-12 10:50:12 +0800

    first push

---
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateUsersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('users', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name');
+            $table->string('icNumber');
+            $table->integer('age');
+            $table->string('gender');
+            $table->string('status');
+            $table->string('email')->unique();
+            $table->string('password');
+            $table->longText('address'); 
+            $table->string('img_path')->default('default.jpg');
+            $table->rememberToken();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('users');
+    }
+}
